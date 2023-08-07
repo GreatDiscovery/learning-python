a48e7641db9af33a4b43a9c79149cc09f5482a1d
jiayun <songbowen@xiaohongshu.com>
2023-08-07 19:55:41 +0800

    update: str format

--- test/str_test.py
+++ test/str_test.py
@@ -6,3 +6,9 @@ class TestStrExample(unittest.TestCase):
         name = "hello"
         age = 15
         print(f"name={name}, age={age}")
+
+    def testFormat(self):
+        run_info = "{\"run_time_info\": {\"cluster_name\": \"fls-review-1/,/fls-review-1\"}}"
+        print(run_info)
+        run_info = "{\"run_time_info\": {\"cluster_name\": \"%s-%s/,/%s-%s\"}}" % ("fls-review", 1, "fls-review", 1)
+        print(run_info)
