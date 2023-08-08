a232e4bb2a6969c960bef1e7c061b7d68d8dbbac
jiayun <songbowen@xiaohongshu.com>
2023-08-08 21:07:21 +0800

    add: Object Oriented Programming

---
+++ test/object/object_test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import unittest
+
+
+class Employee:
+    # '所有员工的基类'
+    empCount = 0
+
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+        Employee.empCount += 1
+
+    def displayCount(self):
+        print("Total Employee %d" % Employee.empCount)
+
+    def displayEmployee(self):
+        print("Name : ", self.name, ", Salary: ", self.salary)
+
+
+class TestStrExample(unittest.TestCase):
+
+    def test_obj_create(self):
+        "创建 Employee 类的第一个对象"
+        emp1 = Employee("Zara", 2000)
+        "创建 Employee 类的第二个对象"
+        emp2 = Employee("Manni", 5000)
+        emp1.displayEmployee()
+        emp2.displayEmployee()
+        print("Total Employee %d" % Employee.empCount)
