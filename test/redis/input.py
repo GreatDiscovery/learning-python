338541a0d3884a863a32430b03cb2fa6947ac7ab
jiayun <songbowen@xiaohongshu.com>
2023-09-02 17:58:16 +0800

    add: redis pipeline输入数据

--- test/redis/input.py
+++ test/redis/input.py
@@ -2,10 +2,10 @@
 import redis
 
 if __name__ == '__main__':
-    source_host = "10.0.21.150"
+    source_host = "127.0.0.1"
     port = 6379
     conn = redis.Redis(host=source_host, port=port, db=0)
-    with open('/Users/jiayun/Downloads/tmp/232.csv', 'r') as file:
+    with open('/Users/jiayun/Downloads/tmp/brand_user_id.sql', 'r') as file:
         lines = file.readlines()
         print(len(lines))
         count = 0
@@ -13,10 +13,12 @@ if __name__ == '__main__':
         batch = 1
         for line in lines:
             count = count + 1
-            pipeline.sadd("mykey", line)
-            if count > 1000:
+            line = line.replace("\n", "")
+            pipeline.sadd("k1", line)
+            if count > 10000:
                 print(f'batch={batch}')
                 pipeline.execute()
                 count = 0
                 batch = batch + 1
                 pipeline = conn.pipeline()
+        pipeline.execute()
