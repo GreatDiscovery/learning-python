5dae674b92ba0ac9e893f47be2f2707f76e3aff2
jiayun <songbowen@xiaohongshu.com>
2023-12-04 19:11:10 +0800

    add: argparse subcommand

--- test/base/argparse_test.py
+++ test/base/argparse_test.py
@@ -1,15 +1,41 @@
 import argparse
 
+
 # python argparse_test.py 1 2 3 --sum
+
+def cmd1Get(args):
+    print("cmd1 get")
+    print(args)
+
+
+def cmd2Set(args):
+    print("cmd2 set")
+    print(args)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('integers', metavar='N', type=int, nargs='+',
-                        help='an integer for the accumulator')
-    parser.add_argument('--sum', dest='accumulate', action='store_const',
-                        const=sum, default=max,
-                        help='sum the integers (default: find the max)')
+    parser = argparse.ArgumentParser()
+    subparser = parser.add_subparsers(dest="subcmd", title="subcmd")
+
+    cmd1 = subparser.add_parser("cmd1", help="cmd1 help")
+    cmd2 = subparser.add_parser("cmd2", help="cmd2 help")
+
+    sub_subparser = cmd1.add_subparsers(dest="subsubcmd", title="subsubcmd")
+    get = sub_subparser.add_parser("get", help="op get")
+    get.set_defaults(func=cmd1Get)
+
+    sub_subparser = cmd2.add_subparsers(dest="subsubcmd", title="subsubcmd")
+    set = sub_subparser.add_parser("set", help="op set")
+    set.set_defaults(func=cmd2Set)
+
+    get.add_argument("--id", help="ID", type=str, required=True)
+    set.add_argument("--id", help="ID", type=str, required=True)
 
     args = parser.parse_args()
-    print(args.accumulate(args.integers))
+
+    if not args.subcmd:
+        parser.print_help()
+    elif not args.subsubcmd:
+        subparser.choices[args.subcmd].print_help()  # 输出子命令的帮助
+    else:
+        args.func(args)
