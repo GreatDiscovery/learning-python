1301e756b35a597036d4e79ded1de5b3132c6f4e
songbowen <1303535630@qq.com>
2025-02-26 23:29:41 +0800

    add: airflow

---
+++ airflow-test/__init__.py
@@ -0,0 +1,5 @@
+# install airflow
+# ./venv/bin/pip3 install "apache-airflow[celery]==2.10.5" --constraint "https://raw.githubusercontent.com/apache/airflow/constraints-2.10.5/constraints-3.8.txt"
+
+# start airflow
+# source ./venv/bin/activate && airflow standalone
